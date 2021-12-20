79dfe1e0466d9cc94de21035ee6466dfd662cc1b
jihed-tatouh <jihedtatouh@tutanota.com>
2021-12-20 15:50:09 +0100

    feat: add `data base connection`

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
 import express, { Application } from "express";
 import { config } from "dotenv";
 
+import { Client } from "pg";
 config();
 const app: Application = express();
 const PORT = Number(process.env.PORT) || 3000;
+
+const client = new Client({
+  connectionString: process.env.DB_URL,
+});
+client.connect();
 
 app.listen(PORT, async () => {
   console.log(` Server: Running on port ${PORT}`);
